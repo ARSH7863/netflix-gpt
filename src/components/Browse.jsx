1da44f6523f9f89b98f627c4e9a7f4f8e6ad86f0
ARSH <arshs287@gmail.com>
2026-01-28 03:28:30 +0530

    Updated Popular Movies Section & created custom hook

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header.jsx";
-import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies.jsx";
+import usePopularMovies from "../hooks/usePopularMovies.jsx";
 import MainContainer from "./MainContainer.jsx";
 import SecondaryContainer from "./SecondaryContainer.jsx";
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
 
   return (
     <div>

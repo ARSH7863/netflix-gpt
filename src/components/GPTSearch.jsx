bdb844ea925e6f3aad0fb1a3f97b0373f5e5fd54
ARSH <arshs287@gmail.com>
2026-01-28 20:46:08 +0530

    GPT Search bar created

--- src/components/GPTSearch.jsx
+++ src/components/GPTSearch.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import GPTSearchBar from "./GPTSearchBar.jsx";
+import GPTMovieSuggestions from "./GPTMovieSuggestions.jsx";
+import { BACKGROUND_IMAGE } from "../utils/constants.jsx";
 
 const GPTSearch = () => {
-  return <div>GPTSearch</div>;
+  return (
+    <div>
+      <div className="">
+        <img
+          className="absolute inset-0 w-full h-full object-cover -z-10"
+          src={BACKGROUND_IMAGE}
+          alt="background"
+        />
+      </div>
+      <GPTSearchBar></GPTSearchBar>
+      <GPTMovieSuggestions></GPTMovieSuggestions>
+    </div>
+  );
 };
 
 export default GPTSearch;
